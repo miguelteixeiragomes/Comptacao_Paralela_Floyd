1c2bcc3e81146902db5245adf052477960b16f25
Miguel Gomes <Miguel Gomes>
2016-10-14 23:23:18 +0100

    commiting...

--- remote_run.py
+++ remote_run.py
@@ -3,4 +3,4 @@ import os
 os.system("git add .")
 os.system('git commit -m "commiting..."')
 os.system('git push origin master')
-os.system('ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin master; mpicc matrix_floyd.c"')
+os.system('ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin master; mpicc matrix_floyd.c -o matrix_floyd.exe"')
