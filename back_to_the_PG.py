48728377e8f8165a85a6f9c379bf01a20a46a072
Miguel Gomes <Miguel Gomes>
2016-10-14 22:32:17 +0100

    commiting...

--- back_to_the_PG.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("git add .")
-os.system('git commit -m "commiting..."')
-os.system('git push origin master')
-os.system('ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin master; mpicc matrix_floyd.c -o matrix_floyd.exe"')
