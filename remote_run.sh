69c4e8745dbc796da62b712ed73ce0e7b12d04d7
Miguel Gomes <Miguel Gomes>
2016-10-14 23:25:15 +0100

    commiting...

---
+++ remote_run.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "commiting..."
+git push origin master
+ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin master; mpicc matrix_floyd.c -o matrix_floyd.exe"
