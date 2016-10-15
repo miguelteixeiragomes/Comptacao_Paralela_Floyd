a3f08cba9855911f6d5600506db29be594fc8458
Miguel Gomes <Miguel Gomes>
2016-10-15 04:24:23 +0100

    commiting...

--- matrix_floyd.c
+++ matrix_floyd.c
@@ -114,7 +114,7 @@ int main(void)
 
 	for (int i = 0; i < N/2; i++){
 		for (int j = 0; j < N/2; j++){
-			printf("%d ", aux2[i*N/2 + j]);
+			printf("%d ", aux1[i*N/2 + j]);
 		}
 		printf("\n");
 	}
