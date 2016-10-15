a56864ad5117f8646a13bd5e5c6c9664529b0750
Miguel Gomes <Miguel Gomes>
2016-10-15 01:52:42 +0100

    commiting...

--- matrix_floyd.c
+++ matrix_floyd.c
@@ -67,9 +67,12 @@ int main(void)
 	floyd_algorithm_2(*m01, *m10, *aux, N/2);
 	floyd_add(*m00, *aux, *m00, N/2);
 
+	/*floyd_algorithm(*m, N);
 	floyd_algorithm(*m, N);
-	floyd_algorithm(*m, N);
-	floyd_algorithm(*m, N);
+	floyd_algorithm(*m, N);*/
+	floyd_algorithm_2(*m, *m, *m, N);
+	floyd_algorithm_2(*m, *m, *m, N);
+	floyd_algorithm_2(*m, *m, *m, N);
 
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
