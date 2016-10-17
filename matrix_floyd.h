2e6d3f1aefd4b018dc58e5e0f363e78fd6bf81f1
Miguel Gomes <Miguel Gomes>
2016-10-18 00:44:25 +0100

    commiting...

--- matrix_floyd.h
+++ matrix_floyd.h
@@ -1,3 +1,10 @@
+#ifndef MATRIX_FLOYD_H
+#define MATRIX_FLOYD_H
+
+
 #define INF 1000000000
 void floyd_algorithm(int*, int*, int*, int);
 void print_matrix(int*, int);
+
+
+#endif
--- read_matrix.h
+++ read_matrix.h
@@ -1,2 +1,9 @@
+#ifndef READ_MATRIX_H
+#define READ_MATRIX_H
+
+
 int read_N(FILE*);
 int* read_matrix(FILE*, int);
+
+
+#endif
