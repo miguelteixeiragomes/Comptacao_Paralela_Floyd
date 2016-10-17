f93b1579031b192bd47b9ae300d996d93001e625
Miguel Gomes <Miguel Gomes>
2016-10-17 22:52:23 +0100

    commiting...

--- matrix_floyd.c
+++ matrix_floyd.c
@@ -79,9 +79,15 @@ int main(void)
 		}
 	}
 
-	for (int step = 0; step < 0; step++) {
+	for (int step = 0; step < 1; step++) {
 		floyd_algorithm(m00, m00, aux, N / 2);
 		floyd_algorithm(m01, m10, aux2, N / 2);
+
+		for (int i = 0; i < N / 2; i++) {
+			for (int j = 0; j < N / 2; j++) {
+			printf("%d ", aux[i*N/2 + j]);
+		}
+
 		min_matrix_floyd(aux, aux2, m00, N / 2);
 		set_zero(aux, N/2);
 		set_zero(aux2, N/2);
