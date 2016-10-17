e973da41894cd53a0313b49e095ff31b27caf81a
Miguel Gomes <Miguel Gomes>
2016-10-18 00:46:52 +0100

    commiting...

--- read_matrix.c
+++ read_matrix.c
@@ -45,9 +45,8 @@ int main(int argc, char **argv)
 	file = fopen(argv[1], "r");
 
 	int N = read_N(file);
-	int* M;
-	M = read_matrix(file, N);
+	int* M = read_matrix(file, N);
 
-	print_matrix(*M, N);
+	print_matrix(M, N);
 	return 0;
 }
