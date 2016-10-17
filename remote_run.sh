ebb3e7ebf28f0ae41a0d9a9723353fb5c1fa9b4d
Miguel Gomes <Miguel Gomes>
2016-10-17 23:45:29 +0100

    commiting...

--- remote_run.sh
+++ remote_run.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "commiting..."
-git push origin test
-ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin test"
+git push origin master
+ssh up201203463@ssh.alunos.dcc.fc.up.pt "cd Documents/Computacao_Paralela_Floyd; git pull origin master"
