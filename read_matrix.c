56d3e0165284ad6c144ede057c7a4989a40fe4bb
Miguel Gomes <Miguel Gomes>
2016-10-18 00:21:07 +0100

    commiting...

---
+++ read_matrix.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix_floyd.h"
+
+
+int read_N(FILE* file) 
+{
+	int N;
+	fscanf(file, "%i", &N);
+	return N;
+}
+
+
+int* read_matrix(FILE *file, int N) {
+	// Allocate matrix pointers to values
+	int *M = malloc(N*N*sizeof(int));
+	// Read from file to matrix
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!fscanf(file, "%i", &M[N*i + j])) {
+				fclose(file);
+				return 0;
+			}
+			if ((i != j) && (M[N*i + j] == 0))
+				M[N*i + j] = INF;
+		}
+	}
+	return M;
+}
+
+void print_output_matrix(int N, int **mat) {
+	for (int i = 0; i<N; i++) {
+		for (int j = 0; j<N - 1; j++) {
+			printf("%i ", mat[i][j]);
+		}
+		printf("%i\n", mat[i][N - 1]);
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	// Check file input
+	if (argc < 2) {
+		printf("No input file supplied\n");
+		return;
+	}
+
+	// Open input file
+	FILE *file;
+	file = fopen(argv[1], "r");
+
+	int N = get_matrix_structure(file);
+	int* M = read_matrix(file, N);
+
+	print_matrix(*M, N);
+	return 0;
+}
