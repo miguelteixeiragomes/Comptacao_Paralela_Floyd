9cdea6e8fac219793f4529970f4f1a7f250d9ef8
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2016-10-21 18:16:54 +0100

    working with scatter, gather and special printing

--- functions.h
+++ functions.h
@@ -9,6 +9,8 @@
 #include <math.h>
 
 int read_N(FILE*);
+int* read_matrix(FILE*, int);
+void print_matrix(int*, int);
 int* read_matrix2(FILE*, int, int);
 void print_matrix2(int*, int, int);
 void floyd_algorithm(int*, int*, int*, int);
