28351303f6b8f4cd4f0bd5c5ccabc32edbe5136f
Miguel Gomes <Miguel Gomes>
2016-11-08 15:51:29 +0000

    commiting...

--- functions.c
+++ functions.c
@@ -77,6 +77,7 @@ int min(int a, int b)
 void floyd_algorithm(int* A, int* B, int* C, int n)
 {
 	int i, j, k;
+	#pragma parallel for private(j, k)
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++)
 			for (k = 0; k < n; k++)
